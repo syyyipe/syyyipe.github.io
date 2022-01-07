2f16938c1b6d6b110f2bd0ae6315f192eb858175
syyyipe <syyyipe@gmail.com>
2022-01-07 14:42:23 +0900

    add pixyll markdown template

--- _site/sw.js
+++ _site/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202201031325",
+        "/css/pixyll.css?202201071439",
         "/"
       ]);
     })
