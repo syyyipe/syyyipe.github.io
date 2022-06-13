0139debad72bc7e7d914336b25863acb5c2475a4
syyyipe <syyyipe@gmail.com>
2022-06-13 09:46:14 +0900

    1486(finished), 1310(still working)

--- _site/sw.js
+++ _site/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202201071610",
+        "/css/pixyll.css?202205120836",
         "/"
       ]);
     })
